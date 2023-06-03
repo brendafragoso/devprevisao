6ee47d3cfb67cc8777d2413e92dc69800a123874
brendafragoso <brendafragoso9@gmail.com>
2023-06-02 22:33:42 -0300

    Projeto DevPrevisão

---
+++ scripts.js
@@ -0,0 +1,38 @@
+//variaveis ==> um espaco da memoria do computador que guardamos algo: número, letra. imagem
+// função ==> um trecho de código que só é exercutado quando é chamado
+
+let chave = "802b166f2e92abe076560d129f554404"
+
+function colocarNaTela(dados){
+console.log(dados)
+
+
+document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
+document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%"
+document.querySelector(".descricao").innerHTML = dados.weather[0].description
+document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function buscarCidade(cidade) { 
+
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" +
+     chave + 
+     "&lang=pt_br" +
+     "&units=metric")
+    .then(resposta => resposta .json())
+  colocarNaTela(dados)
+}
+
+
+
+function cliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
+
+
+
